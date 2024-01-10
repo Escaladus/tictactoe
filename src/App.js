38c7a56fe918d95e0ddf05a25e2da03ef5a6464a
Denis Eskelinen <escaladus@gmail.com>
2024-01-10 21:20:47 +0200

    5 minutes game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+const initialValue = {
+  0: "",
+  1: "",
+  2: "",
+  3: "",
+  4: "",
+  5: "",
+  6: "",
+  7: "",
+  8: "",
+};
 function App() {
+  const [state, setState] = useState(false);
+  const [value, setValue] = useState(initialValue);
+
+  const RenderNode = () => {
+    const nods = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+    const handleClick = (num) => {
+      if (value[num] === "") {
+        setValue((prev) => ({ ...prev, [num]: state ? "X" : "O" }));
+        setState(!state);
+      } else {
+        alert("Next please!");
+      }
+    };
+
+    return nods.map((nod) => {
+      return (
+        <div key={nod}>
+          <button onClick={() => handleClick(nod)}>{value[nod]}</button>
+        </div>
+      );
+    });
+  }; // RenderNode
+
+  const Reset = () => {
+    return (
+      <div>
+        <button onClick={() => setValue(initialValue)}>Reset</button>
+      </div>
+    );
+  }; // Reset
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RenderNode />
+      <Reset />
     </div>
   );
-}
+} // App
 
 export default App;
